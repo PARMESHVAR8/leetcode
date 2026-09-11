48ffe0ab3f1d701c0236c4150fc60599a4079329
Parmeshvar <parmeshvarbugaliya86@gmail.com>
2026-09-11 23:57:57 +0530

    Time: 3 ms (46.34%), Space: 46.3 MB (60.68%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        int a = prices[0]+prices[1];
+        if(a<=money){
+            return money-a;
+        }else{
+            return money;
+        }
+    }
+}
