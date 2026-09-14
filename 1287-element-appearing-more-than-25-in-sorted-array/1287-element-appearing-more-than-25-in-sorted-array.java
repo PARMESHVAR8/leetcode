fa5d8ad7f6616f8c72979c3fb42338aa355cd480
Parmeshvar <parmeshvarbugaliya86@gmail.com>
2026-09-14 23:34:22 +0530

    Time: 1 ms (68.04%), Space: 46.2 MB (83.75%) - LeetHub

---
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        int count =1;
+        for(int i =1; i<arr.length; i++){
+            if(arr[i]==arr[i-1]){
+                count++;
+            }else{
+                count=1;
+            }
+            if(count*4>arr.length){
+                return arr[i];
+            }
+        }return arr[0];
+
+    }
+}
